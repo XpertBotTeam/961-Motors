342249338a980176e01a69fa7511295482854b4f
Michael El Hajj <crpmmn@gmail.com>
2022-08-17 00:23:26 +0300

    updated2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="Username" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="Username" type="Username" class="form-control @error('Username') is-invalid @enderror" name="Username" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('Username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://i7.pngguru.com/preview/659/143/606/partnership-computer-icons-clip-art-deal-with-it.jpg" class="rounded-circle" style="height: 150px;">
+            <div class="pt-5"><strong>Sell my car</strong></div>
+            <div class="pt-3"><strong>Buy a car</strong></div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="https://tse3.mm.bing.net/th?id=OIP.BjHEx8-mD-5-_508oHFZUQHaFj&pid=Api&P=0" class="w-100">   
+        </div>
+        <div class="col-4">
+            <img src="https://tse2.mm.bing.net/th?id=OIP.QlrIik_jJA5hvDKLkmCHBQHaFj&pid=Api&P=0" class="w-100">
+    </div>
+    <div class="col-4">
+        <img src="https://tse4.mm.bing.net/th?id=OIP.d97_bw1dJ54V1ZpvVo-NrgHaFj&pid=Api&P=0" class="w-100">
 </div>
 @endsection
